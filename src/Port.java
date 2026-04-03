b6cba5ad1568b1e89917a0199226593cd9601782
Ratboy-Junior <mcrathman2005@gmail.com>
2026-04-03 16:25:39 -0600

    Added comments

--- src/Port.java
+++ src/Port.java
@@ -1,11 +1,23 @@
+/** Class for the Port object, which holds and integer value. */
 public class Port {
+    
     private int val;
 
+    /** Creates an instance of a Port object. */
     public Port() {}
 
+    /**
+     * Sets the value the Port holds.
+     * @param val The new value the Port holds.
+     */
     public void setVal(int val) {
         this.val = val;
     }
+
+    /**
+     * Fetches the value the Port currently holds.
+     * @return The Port's held value.
+     */
     public int getVal() {
         return val;
     }
